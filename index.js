4ef012c9ff6d8fc88726d615564a8324e2bf981a
Rima Akter <rimawahid3@gmail.com>
2021-04-07 22:52:45 +0600

    port updated

--- index.js
+++ index.js
@@ -76,6 +76,4 @@ app.post('/addBuyData', (req, res) => {
 
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || port)
