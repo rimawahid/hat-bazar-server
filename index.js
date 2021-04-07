5feb5d3b8a5f7d8bff3baf50f2e036e2410924ad
Rima Akter <rimawahid3@gmail.com>
2021-04-07 22:35:24 +0600

    my server code

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const cors = require('cors')
+const bodyParser = require('body-Parser')
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+console.log(process.env.DB_USER, process.env.DB_PASS)
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kdwca.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    //console.log('connection err', err)
+  const adminCollection = client.db("product").collection("admin");
+    console.log('admin collection successfully')
+
+    const clientCollection = client.db("product").collection("client");
+
+    app.get('/product', (req, res) => {
+      adminCollection.find()
+      .toArray((err,items) => {
+        res.send(items)
+        console.log('from database', items)
+      })
+    })
+
+    app.post ('/addProduct', (req, res) => {
+        const newProduct = req.body;
+        console.log('adding new product', newProduct)
+        adminCollection.insertOne(newProduct)
+        .then(result => {
+        console.log('inserted count',result.insertedCount)
+        res.send(result.insertedCount > 0)
+        })
+    })
+
+    app.delete('/deleteProduct/:id', (req, res) => {
+      const id = ObjectID(req.params.id);
+      console.log('delete this', id);
+      adminCollection.findOneAndDelete({_id: id})
+      .then(result => {
+        console.log(result)
+        res.send(result.deletedCount > 0)
+      })
+  })
+
+  app.get('/buyData', (req, res) => {
+    clientCollection
+        .find({ email: req.query.email })
+        .toArray((err, result) => {
+            res.send(result);
+        });
+});
+
+app.post('/addBuyData', (req, res) => {
+    const newfood = req.body;
+    clientCollection.insertOne(newfood).then((result) => {
+        res.send(result.insertedCount > 0);
+    });
+});
+
+  //client.close();
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
