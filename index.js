9abbcaccf8017f3d8c9fd5b2096e76a0c88900ee
Rima Akter <rimawahid3@gmail.com>
2021-04-08 09:21:39 +0600

    as

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ app.get('/', (req, res) => {
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kdwca.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kdwca.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://hatBazar:bazarHat21@cluster0.kdwca.mongodb.net/product?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     //console.log('connection err', err)
